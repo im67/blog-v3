f49ce6ec7f0f4ab7f617df093c7d5294542a134b
im6767 <im6767@im6767deMacBook-Pro.local>
2025-08-12 16:39:43 +0800

    fix: docs build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  content: [
+    '.vitepress/theme/**/*.{vue,js,ts,jsx,tsx,md}',  // VitePress 内容
+    './content/**/*.md',
+    './**/*.md',                   
+  ],
   plugins: [
     require('@tailwindcss/line-clamp'),
     // 其他插件
