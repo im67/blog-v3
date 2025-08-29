0ebada6cce89a9d13f02ac4121534a3b6a7b47fa
im6767 <im6767@im6767deMacBook-Pro.local>
2025-08-29 11:34:46 +0800

    feat: 添加博客文章

---
+++ .vitepress/theme/views/components/DateClock.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import * as d3 from "d3";
+
+onMounted(() => {
+  d3.select('clock-svg');
+})
+</script>
+
+<template>
+  <svg id="clock-svg" :width="600" :height="600" style="overflow: visible;"></svg>
+</template>
+
+<style lang="scss" scoped></style>
